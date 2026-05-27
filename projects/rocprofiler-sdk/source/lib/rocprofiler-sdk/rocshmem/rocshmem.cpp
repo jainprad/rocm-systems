7890c3abbb61a30e20e7b95a3bbff57fb1dd4b79
EvanPhoukong <Evan.Phoukong@amd.com>
2026-05-27 14:37:16 +0000

    Fix incorrect assignment of ancestor correlation ID

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/rocshmem/rocshmem.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/rocshmem/rocshmem.cpp
@@ -172,7 +172,7 @@ rocshmem_api_impl<TableIdx, OpIdx>::functor(Args... args)
     auto  tracer_data      = common::init_public_api_struct(callback_api_data_t{});
     auto* corr_id          = tracing::correlation_service::construct(ref_count);
     auto  internal_corr_id = corr_id->internal;
-    auto  ancestor_corr_id = corr_id->internal;
+    auto  ancestor_corr_id = corr_id->ancestor;
 
     tracing::populate_external_correlation_ids(external_corr_ids,
                                                thr_id,
